d3e0c5e660d58dd24e81e67a45bf67df578245e1
Pratap Tyagi <tyagipratap111@gmail.com>
2021-04-22 00:22:26 +0530

    Basic outlook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Post from "./components/Post/Post";
+import Image from "./ascets/react.jfif";
 
 const App = () => {
+  const data = {
+    username: "1",
+    caption: "Hello 1",
+    imageUrl: `${Image}`,
+  };
+
   return (
     <div className="app">
       <Header />
+      <Post data={data} />
     </div>
   );
 };
